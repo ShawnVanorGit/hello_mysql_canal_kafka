db197ab058831e63ce92169479f8face1cd18778
shawnvan <13631230887@163.com>
2020-09-13 22:39:37 +0800

    first commit

---
+++ src/main/java/GlobalConfigUtil.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * 配置文件的公共类
+ */
+public class GlobalConfigUtil {
+
+    //读取application.properties文件
+    private static ResourceBundle resourceBundle = ResourceBundle.getBundle("application");
+
+    public static String canalHost= resourceBundle.getString("canal.host");
+    public static String canalPort = resourceBundle.getString("canal.port");
+    public static String canalInstance = resourceBundle.getString("canal.instance");
+    public static String mysqlUsername = resourceBundle.getString("mysql.username");
+    public static String mysqlPassword=  resourceBundle.getString("mysql.password");
+    public static String kafkaBootstrap= resourceBundle.getString("kafka.bootstrap.servers");
+    public static String kafkaZookeeper= resourceBundle.getString("kafka.zookeeper.connect");
+    public static String kafkaInput = resourceBundle.getString("kafka.input.topic");
+
+    public static void main(String[] args) {
+        System.out.println(canalHost);
+    }
+}
